bcab774157f04805a99267b8a7acdb237e18257c
Luiz Guilherme Pais dos Santos <luizgpsantos@gmail.com>
2019-04-27 09:52:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 var app = require('express')()
 
-
 app.get('/', function (req, res) {
 
-var err = new Error('Error generated')
-
-//error generated and captured
-apm.captureError(err)
+    //error generated and captured
+    var err = new Error('Error generated')
+    apm.captureError(err)
  
-res.send('Hello Elastic Lovers')
+    res.send('Hello Elastic Lovers')
 
- 
- //created error without capture
- my_error()
-
-  
+    //created error without capture
+    my_error()
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
